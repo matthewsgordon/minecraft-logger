33a864bab93e5599a95f04cef06cdc346c82e955
Matthew Gordon <matthewsg210703@gmail.com>
2025-04-27 21:03:12 +0100

    Updated with trial Id

--- src/main/java/uk/ac/edgehill/mres/App.java
+++ src/main/java/uk/ac/edgehill/mres/App.java
@@ -6,18 +6,18 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDateTime;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Score;
@@ -25,90 +25,40 @@ import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 
 public class App extends JavaPlugin implements Listener {
+     Logger trackerLogger = Bukkit.getLogger();
 
-    private boolean stopRepeater = true;
+    boolean stopRepeater = true;
 
-    private String getTrialStartLocation(double x) {
-        switch ((int) (x)) {
-            case 91:
-                return "N";
-            case 102:
-                return "E";
-            case 104:
-                return "W";
-            case 114:
-                return "S";
-            default:
-                return null;
-        }
+    public void onEnable() {
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        this.trackerLogger.info("TRACKING PLUGIN INITIALISING");
     }
 
-    @Override
-    public void onEnable() {
-        getLogger().info("Starting event logging service...");
-        // Register the event listener
-        getServer().getPluginManager().registerEvents(this, this);
+    public void onDisable() {
+        this.trackerLogger.info("SHUTTING DOWN TRACKER...");
     }
 
     @EventHandler
     public void onLogin(PlayerJoinEvent event) {
         final Player thePlayer = event.getPlayer();
         this.stopRepeater = true;
-        Location playerSpawnLocation = new Location(thePlayer.getWorld(), thePlayer.getLocation().getBlockX(),
-                thePlayer.getLocation().getBlockY(), thePlayer.getLocation().getBlockZ());
-        getLogger().info(event.getPlayer().getName() + " has logged in");
-        getLogger().info(
-                "Welcome " + event.getPlayer().getName() + "!" + "Your current position is: " + playerSpawnLocation);
+        Location playerSpawnLocation = new Location(thePlayer.getWorld(), thePlayer.getLocation().getBlockX(), thePlayer.getLocation().getBlockY(), thePlayer.getLocation().getBlockZ());
+        this.trackerLogger.info(event.getPlayer().getName() + " has logged in");
+        this.trackerLogger.info("Welcome " + event.getPlayer().getName() + "!" + "Your current position is: " + playerSpawnLocation);
         thePlayer.sendMessage("Welcome to the TseLab Event Arena Simulation");
         Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(this, () -> {
-            if (this.stopRepeater)
-                logToFile(thePlayer, thePlayer.getLocation());
-            scoreboardlogtoFile(thePlayer);
+            if (App.this.stopRepeater)
+                App.this.logToFile(thePlayer, thePlayer.getLocation());
+            App.this.scoreboardlogtoFile(thePlayer);
         }, 0L, 20L);
         ScoreboardManager sm = Bukkit.getScoreboardManager();
         Scoreboard s = sm.getMainScoreboard();
     }
 
-    public void logToFile(Player currentPlayer, Location playerCurrentLocation) {
-        try {
-            File dataFolder = getDataFolder();
-            if (!dataFolder.exists())
-                dataFolder.mkdir();
-            File saveTo = new File(getDataFolder(), currentPlayer.getPlayer().getName() + "_location.csv");
-            if (!saveTo.exists()) {
-                saveTo.createNewFile();
-                FileWriter fw = new FileWriter(saveTo, true);
-                PrintWriter pw = new PrintWriter(fw);
-                pw.println(
-                        "Date Time,Player Location X,Player Location Y,Player Location Z," +
-                                "Player Pitch,Player Yaw,Looking At,Looking X,Looking Y,Looking Z," +
-                                "Ground Height");
-                pw.close();
-            }
-
-            int groundHeight = currentPlayer.getWorld().getHighestBlockYAt(playerCurrentLocation);
-            Block currentWalkingBlock = currentPlayer.getWorld().getBlockAt(playerCurrentLocation.getBlockX(),
-                    playerCurrentLocation.getBlockY() - 1, playerCurrentLocation.getBlockZ());
-
-            LocalDateTime timenow = LocalDateTime.now();
-            FileWriter fw = new FileWriter(saveTo, true);
-            PrintWriter pw = new PrintWriter(fw);
-
-            String lookingAtInfo = ",,,";
-            Block targetBlock = currentPlayer.getTargetBlock(null, 100);
-            if (targetBlock != null) {
-                lookingAtInfo = targetBlock.getType().toString() + "," +
-                        targetBlock.getX() + "," + targetBlock.getY() + "," + targetBlock.getZ();
-            }
-
-            pw.println(timenow + "," + playerCurrentLocation.getBlockX() + "," +
-                    playerCurrentLocation.getBlockY() + "," + playerCurrentLocation.getBlockZ() + "," +
-                    playerCurrentLocation.getPitch() + "," + playerCurrentLocation.getYaw() + "," +
-                    lookingAtInfo + "," + groundHeight);
-            pw.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        this.stopRepeater = false;
+        this.trackerLogger.info(event.getPlayer().getName() + " has left the game");
     }
 
     public void scoreboardlogtoFile(Player currentPlayer) {
@@ -142,83 +92,94 @@ public class App extends JavaPlugin implements Listener {
         Score scoreDWELL_C_L5 = objectiveDWELL.getScore("C_L5");
         Score scoreDWELL_C_L6 = objectiveDWELL.getScore("C_L6");
 
+
         try {
             File scoreboardFolder = getDataFolder();
             if (!scoreboardFolder.exists())
                 scoreboardFolder.mkdir();
-            File scoreboardsaveTo = new File(getDataFolder(),
-                    currentPlayer.getPlayer().getDisplayName() + "_scoreboard.csv");
-            if (!scoreboardsaveTo.exists()) {
+            File scoreboardsaveTo = new File(getDataFolder(), currentPlayer.getPlayer().getDisplayName() + "_scoreboard.log");
+            if (!scoreboardsaveTo.exists())
                 scoreboardsaveTo.createNewFile();
-                FileWriter fw = new FileWriter(scoreboardsaveTo, true);
-                PrintWriter pw = new PrintWriter(fw);
-                pw.println(
-                        "Date Time,No. of Errors,Time Errors,Trial Tracker,Distance," +
-                                "Explore A1,Explore A2,Explore A3,Explore A4,Explore A5,Explore A6," +
-                                "Explore B1,Explore B2,Explore B3,Explore B4,Explore B5,Explore B6," +
-                                "Explore C1,Explore C2,Explore C3,Explore C4,Explore C5,Explore C6");
-                pw.close();
-            }
-
-            LocalDateTime timenow = LocalDateTime.now();
+            Date nowDate = new Date();
+            SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
             FileWriter fw = new FileWriter(scoreboardsaveTo, true);
             PrintWriter pw = new PrintWriter(fw);
-            pw.println(timenow + "," + scoreE.getScore() + "," + scoreT.getScore() + ","
-                    + scoreTN.getScore() + "," + scoreD.getScore() + "," + scoreDWELL_A_L1.getScore() + ","
-                    + scoreDWELL_A_L2.getScore() + "," + scoreDWELL_A_L3.getScore() + "," + scoreDWELL_A_L4.getScore()
-                    + "," + scoreDWELL_A_L5.getScore() + "," + scoreDWELL_A_L6.getScore() + ","
-                    + scoreDWELL_B_L1.getScore() + "," + scoreDWELL_B_L2.getScore() + "," + scoreDWELL_B_L3.getScore()
-                    + "," + scoreDWELL_B_L4.getScore() + "," + scoreDWELL_B_L5.getScore() + ","
-                    + scoreDWELL_B_L6.getScore() + "," + scoreDWELL_C_L1.getScore() + ","
-                    + scoreDWELL_C_L2.getScore() + "," + scoreDWELL_C_L3.getScore() + ","
-                    + scoreDWELL_C_L4.getScore() + "," + scoreDWELL_C_L5.getScore() + ","
-                    + scoreDWELL_C_L6.getScore());
+            pw.println(format.format(nowDate) + " Error: " + scoreE.getScore() + " Time: " + scoreT.getScore() + " Trial_Tracker: " + scoreTN.getScore() + " Distance: " + scoreD.getScore() + " Explore_A1: " + scoreDWELL_A_L1.getScore() + " Explore_A2: " + scoreDWELL_A_L2.getScore() + " Explore_A3: " + scoreDWELL_A_L3.getScore() + " Explore_A4: " + scoreDWELL_A_L4.getScore() + " Explore_A5: " + scoreDWELL_A_L5.getScore() + " Explore_A6: " + scoreDWELL_A_L6.getScore() + " Explore_B1: " + scoreDWELL_B_L1.getScore() + " Explore_B2: " + scoreDWELL_B_L2.getScore() + " Explore_B3: " + scoreDWELL_B_L3.getScore() + " Explore_B4: " + scoreDWELL_B_L4.getScore() + " Explore_B5: " + scoreDWELL_B_L5.getScore() + " Explore_B6: " + scoreDWELL_B_L6.getScore() + " Explore_C1: " + scoreDWELL_C_L1.getScore() + " Explore_C2: " + scoreDWELL_C_L2.getScore() + " Explore_C3: " + scoreDWELL_C_L3.getScore() + " Explore_C4: " + scoreDWELL_C_L4.getScore() + " Explore_C5: " + scoreDWELL_C_L5.getScore() + " Explore_C6: " + scoreDWELL_C_L6.getScore());
+            // pw.println(format.format(nowDate) + "EA_Distance: " + Bukkit.getScoreboardManager().getMainScoreboard().getObjective("EA_Distance").getScore("NeuronSafari") + " Errors: " + currentPlayer.getScoreboard().getObjective("Errors").getScore("NeuronSafari") + " Time_Taken: " + currentPlayer.getScoreboard().getObjective("Time_Taken").getScore("NeuronSafari"));
+            //pw.println(format.format(nowDate) + "EA_Distance: " + currentPlayer.getPlayer().getScoreboard().getScores("EA_Distance") + " Errors: " + currentPlayer.getPlayer().getScoreboard().getScores("Errors") + " Time_Taken: " + currentPlayer.getPlayer().getScoreboard().getScores("Time_Taken"));
+            pw.close();
+        }   catch (IOException e) {
+            e.printStackTrace();
+        }       
+    }
+    public void logToFile(Player currentPlayer, Location playerCurrentLocation) {
+        try {
+            File dataFolder = getDataFolder();
+            if (!dataFolder.exists())
+                dataFolder.mkdir();
+            File saveTo = new File(getDataFolder(), currentPlayer.getPlayer().getName() + "_location.log");
+            if (!saveTo.exists())
+                saveTo.createNewFile();
+            int groundHeight = currentPlayer.getWorld().getHighestBlockYAt(playerCurrentLocation);
+            Block currentWalkingBlock = currentPlayer.getWorld().getBlockAt(playerCurrentLocation.getBlockX(), playerCurrentLocation.getBlockY() - 1, playerCurrentLocation.getBlockZ());
+
+            Date nowDate = new Date();
+            SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+            FileWriter fw = new FileWriter(saveTo, true);
+            PrintWriter pw = new PrintWriter(fw);
+
+            String lookingAtInfo = "None";
+            Block targetBlock = currentPlayer.getTargetBlock(null, 100);
+            if (targetBlock != null) {
+                lookingAtInfo = targetBlock.getType() + " at (" +
+                        targetBlock.getX() + ", " + targetBlock.getY() + ", " + targetBlock.getZ() + ")";
+            }
+
+            pw.println(format.format(nowDate) + " CurrentLocation(x,y,z): " + playerCurrentLocation.getBlockX() + " " +
+                    playerCurrentLocation.getBlockY() + " " + playerCurrentLocation.getBlockZ() + " Head_Direction: " +
+                    playerCurrentLocation.getPitch() + " " + playerCurrentLocation.getYaw() + " Looking At: " + lookingAtInfo);
             pw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
 
-    @EventHandler
-    public void onPlayerTeleport(PlayerTeleportEvent event) {
-        // Get the player who teleported
-        Player player = event.getPlayer();
-        // Get the from and to locations
-        Location to = event.getTo();
-        String startLocation = getTrialStartLocation(to.getX());
-        // Log the teleport event
-        getLogger().info(player.getName() + " teleported at " + startLocation);
-    }
-
-    @EventHandler
-    public void onEntityPickupItem(EntityPickupItemEvent event) {
-        // Check if the entity is a player
-        if (event.getEntity() instanceof Player) {
-            Player player = (Player) event.getEntity();
-            Item item = event.getItem();
-            if (item.getName().contains("Concrete Powder")) {
-                getLogger().info("Picked up " + item.getName());
-            }
-            getLogger().info(System.currentTimeMillis() + ": " + player.getName() + " picked up " + item.getName());
-        }
     }
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
+        ScoreboardManager manager = Bukkit.getScoreboardManager();
+        Scoreboard board = manager.getMainScoreboard();
+        Objective objectiveD = board.getObjective("distanceWalked");
+        Objective objectiveE = board.getObjective("Error");
+        Objective objectiveT = board.getObjective("Info");
+        Score scoreD = objectiveD.getScore("Distance_Travelled");
+        Score scoreE = objectiveE.getScore("Error_Tracker");
+        Score scoreT = objectiveT.getScore("EA_TimeSECS");
         // Check if the action is a right-click on a block
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             Block clickedBlock = event.getClickedBlock();
             // Check if the block is a button
             if (clickedBlock != null && (clickedBlock.getType() == Material.STONE_BUTTON
                     || clickedBlock.getType() == Material.OAK_BUTTON)) {
-                getLogger().info(
-                        System.currentTimeMillis() + ": " + event.getPlayer().getName());
+                getLogger().info(System.currentTimeMillis() + ": " + event.getPlayer().getName());
+                try {
+                    File scoreboardFolder = getDataFolder();
+                    if (!scoreboardFolder.exists())
+                        scoreboardFolder.mkdir();
+                    File scoreboardsaveTo = new File(getDataFolder(), event.getPlayer().getDisplayName() + "_quickstats.csv");
+                    if (!scoreboardsaveTo.exists())
+                        scoreboardsaveTo.createNewFile();
+                    Date nowDate = new Date();
+                    SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+                    FileWriter fw = new FileWriter(scoreboardsaveTo, true);
+                    PrintWriter pw = new PrintWriter(fw);
+                    pw.println(format.format(nowDate) + " Error: " + scoreE.getScore() + " Time: " + scoreT.getScore() + " Distance: " + scoreD.getScore());
+                    pw.close();
+                }   catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
-    @Override
-    public void onDisable() {
-        getLogger().info("Stopping event logging service...");
-    }
 }
